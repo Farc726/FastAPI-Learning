57f7f680bbaa045016b1cf2920f4eb52d5be828c
Farc726 <2177898823@qq.com>
2026-08-18 12:09:41 +0800

    成功运行FastAPI第一个程序 并学习了路由相关知识

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
 
+# 创建FastAPI实例
 app=FastAPI()
 
+#通过GET方法 访问根路径"/"
 @app.get("/")
-def root():
-    return {"message":"Hello World"}
+
+#定义路径执行函数
+#异步函数
+async def root():
+# 返回响应内容
+    return {"message":"Hello World666"}
+
+# 通过GET方法 访问"/hello"路径
+@app.get("/hello")
+# 定义函数
+async def my_hello():
+    #相应内容
+    return {"msg":"开启我的FastAPI之旅!"}
+
+@app.get("/user/hello")
+async def first():
+    return {"msg":"我正在学习FastAPI！一定会成功的！"}
+
+
+
