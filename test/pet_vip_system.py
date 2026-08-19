7c815d308d623cd3a429eb06f58ddc590e68f29c
Farc726 <2177898823@qq.com>
2026-08-19 17:58:44 +0800

    修改完毕！ 1.在修饰器中声明响应模型即可（自动过滤）列表亦然但要注意加list[] 2.当响应是一个"组合结构"时，需要定义专用输出模型--涉及自定义响应类型知识点

--- test/pet_vip_system.py
+++ test/pet_vip_system.py
@@ -8,7 +8,7 @@ class MemberBase(BaseModel):
     email:EmailStr
     full_name:str|None=None
     
-class MenberCreate(MemberBase):
+class MemberCreate(MemberBase):
     password:str=Field(...,min_length=6,max_length=20)
     
 class MemberOut(MemberBase):
@@ -32,8 +32,8 @@ Member_List=[]
 next_id=1
 
 #注册
-@app.post("/members")
-async def create_member(member:MenberCreate):
+@app.post("/members",response_model=MemberOut)
+async def create_member(member:MemberCreate):
 # 声明全局对象next_id
     global next_id
 #将MemberCreate对象转为字典
@@ -44,12 +44,12 @@ async def create_member(member:MenberCreate):
 #构造Member 对象存进列表
     new_member=Member(id=next_id,username=new_member_dict["username"],email=new_member_dict["email"],full_name=new_member_dict["full_name"],password=new_member_dict["password"])
     Member_List.append(new_member)
+    next_id += 1  
 # 构建MemberOut类的对象 因为其中有model_config = ConfigDict(from_attributes=True)
 # 直接传入类可以自动过滤
 # 这个为什么不能直接在post中限定直接输出过滤后的new_member呢？
-    memberout=MemberOut.model_validate(new_member)
-    next_id+=1
-    return memberout
+# 完全可以！！！
+    return new_member
 
 
 # 查询单个
@@ -61,22 +61,27 @@ async def get_member(member_id:int=Path(...,ge=1,le=1000)):
     raise HTTPException(404,detail="会员不存在~")
 
 # 会员列表
-@app.get("/members")
+@app.get("/members",response_model=list[MemberOut])
 async def get_all_members():
-    result=[]
-    for member in Member_List:
-        memberout=MemberOut.model_validate(member)
-        result.append(memberout)
-    return result
+    return Member_List
 # 继承BaseModel的类哪怕在列表内也可以自动转化后输出？？
+# 解答：
+# 1.如果列表里存的是 Pydantic 模型实例（比如 MemberOut 对象）——不写 response_model 也能自动序列化输出。
+# 2.如果列表里存的是自定义类对象（Member_List 就是这种情况）——只要声明响应模型，FastAPI 会逐个元素做转换
+# 所以 上面代码还可以优化！
 
 # 登录
-@app.post("/members/login")
+# 再修改 --涉及自定义响应类型知识点
+# 定义登录响应的输出模型
+class LoginOut(BaseModel):
+    msg:str
+    member:MemberOut
+
+@app.post("/members/login",response_model=LoginOut)
 async def login(login:LoginIn):
     for member in Member_List:
         if login.username==member.username and login.password==member.password:
-            memberout=MemberOut.model_validate(member)
-            return {"msg":"登录成功","member":memberout}
+            return {"msg":"登录成功","member":member}
     raise HTTPException(401,detail="用户名或密码错误~")
 
     
