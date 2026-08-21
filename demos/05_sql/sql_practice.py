329cb329f04c52b0fdf97ed343297d3b66b1a5f7
Farc726 <2177898823@qq.com>
2026-08-21 16:55:34 +0800

    补充聚合函数与聚合函数+WHERE 相关知识点

--- demos/05_sql/sql_practice.py
+++ demos/05_sql/sql_practice.py
@@ -33,9 +33,9 @@ CREATE TABLE IF NOT EXISTS students (
 
 # 2.查询数据
 #2.1查询全部
-cur.execute("""
-SELECT * FROM students
-            """)
+# cur.execute("""
+# SELECT * FROM students
+#             """)
 
 #2.2只查询某几列
 # cur.execute("""
@@ -51,6 +51,13 @@ SELECT * FROM students
 # SELECT * FROM students
 # ORDER BY chinese DESC
 #             """)
+
+# 前面两个知识点结合
+# cur.execute("""
+# SELECT name,chinese + math + english AS total FROM students
+# WHERE chinese>=95
+# ORDER BY total DESC    
+#         """)
 #2.5只查询前n条
 # cur.execute("""
 # SELECT * FROM students
@@ -68,19 +75,54 @@ SELECT * FROM students
 # cur.execute("""
 # UPDATE students
 # SET chinese=100
-# WHERE name=='张三'
+# WHERE name='张三'
 #             """)
 # conn.commit()
 
 #5.删除 DELETE
 # cur.execute("""
 # DELETE FROM students
-# WHERE name=='王五'    
+# WHERE name='王五'    
 #         """)
-# conn.commit()
-for row in cur.fetchall():
-    print(row)
+## conn.commit()
+# for row in cur.fetchall():
+#     print(row)
 
+# 6. 关于聚合函数
+# COUNT(*)：统计一共有多少学生
+res1=cur.execute("SELECT COUNT(*) FROM students;").fetchone()
+print("学生总人数为：",res1)
 
+# AVG：语文平均分
+res2=cur.execute("SELECT AVG(chinese) FROM students;").fetchone()
+print("语文平均分为：",res2)
+
+# MAX：数学最高分
+res3=cur.execute("SELECT MAX(math) FROM students;").fetchone()
+print("数学最高分为：",res3)
+
+# MIN：英语最低分
+res4=cur.execute("SELECT MIN(english) FROM students;").fetchone()
+print("英语最低分为：",res4)
+
+# SUM：数学全部同学分数总和
+res5 = cur.execute("SELECT SUM(math) FROM students;").fetchone()
+print("数学总分总和", res5)
+
+# 7.聚合函数+WHERE多条件版
+# BETWEEN：数学分数在 80 ~ 90（包含两端）
+r1 = cur.execute("""
+SELECT name,math FROM students 
+WHERE math BETWEEN 80 AND 90;
+""").fetchall()
+print("\n数学80~90：", r1)
+
+# LIKE模糊查询：名字以"张"开头
+r2 = cur.execute("SELECT name FROM students WHERE name LIKE '张%';").fetchall()
+print("姓张的同学：", r2)
+
+# AND 同时满足两个条件：语文>=85 并且 英语>=85
+r3 = cur.execute("SELECT name FROM students WHERE chinese >=85 AND english >=85;").fetchall()
+print("语文≥85且英语≥85：", r3)
 # 关闭连接
 conn.close()
